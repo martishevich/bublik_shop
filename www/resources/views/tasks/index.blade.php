1813058d3f113222ff1c358752a089e918a0e416
Eugene Kozlov <cyberkekc@gmail.com>
2018-11-20 10:18:14 +0300

    Firs commit

---
+++ www/resources/views/tasks/index.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Tasks</title>
+</head>
+<body>
+<ul>
+    @foreach($tasks as $task)
+        <li>
+            <a href="tasks/{{$task->id}}">{{$task->body}}</a>
+        </li>
+    @endforeach
+</ul>
+</body>
