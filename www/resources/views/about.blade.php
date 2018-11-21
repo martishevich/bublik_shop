2f479141f1ba8c3c382852f5231894596afd61bb
vkoyda <gbeleptica@gmail.com>
2018-11-21 16:41:07 +0300

    Page about done

---
+++ www/resources/views/about.blade.php
@@ -0,0 +1,19 @@
+@extends('layout')
+
+@section('content')
+
+<div class="aboutus">
+    <div class="whow">
+    <h2>
+    Hello! 
+    <br>
+We are pleased to introduce our company to you!
+<br>
+Bublik company has been selling computers and components for them for more than 15 years.
+<br>Any of our employees will be happy to help you solve any problem related to the purchase of a computer and accessories.
+</h2>
+    </div>
+    
+</div>
+
+@endsection
--- www/routes/web.php
+++ www/routes/web.php
@@ -12,3 +12,4 @@
 */
 
 Route::get('/', 'PostsController@index');
+Route::get('/about', 'AboutController@about');
