b328b2a44bf0bc84c0178220a80a2d4159147852
Eugene Kozlov <cyberkekc@gmail.com>
2018-11-22 13:23:19 +0300

    product model+migration+controller

--- www/database/migrations/2018_11_22_082655_create_products_table.php
+++ www/database/migrations/2018_11_22_082655_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCategoriesTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,14 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title', 50);
-            $table->string('description', 250);
-            $table->mediumInteger('position')->default('0');
+            $table->unsignedInteger('category_id');
+            $table->string('name', 50);
+            $table->text('description');
+            $table->string('short_disc', 150);
+            $table->float('price', 5, 2);
+            $table->boolean('is_active')->default('1');
             $table->timestamps();
         });
     }
@@ -29,6 +32,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('products');
     }
 }
