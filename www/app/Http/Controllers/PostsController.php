9f9b94bec77b7d6267f528cfea7f423692d4f62d
Vladislav <periant@mail.ru>
2018-11-26 21:52:50 +0300

    add session cart

--- www/app/Http/Controllers/PostsController.php
+++ www/app/Http/Controllers/PostsController.php
@@ -6,10 +6,16 @@ use App\Categories;
 use Illuminate\Http\Request;
 use App\Http\Controllers;
 use App\Providers;
+use mysql_xdevapi\Session;
 
 class PostsController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
+
+        $id = $request['id'];
+        $value = $request->session()->get('cart.'.$id, 0);
+        $request->session()->put('cart.'.$id, $value+1);
+
         $catTitle = Categories::orderBy('position')
             ->get();
         return view('posts.index', compact('catTitle'));
