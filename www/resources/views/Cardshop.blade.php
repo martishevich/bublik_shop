a2cd637b6dfbc89dbdc4220de354b5378613a53e
vkoyda <gbeleptica@gmail.com>
2018-11-27 12:38:52 +0300

    CartShop

--- www/resources/views/Cardshop.blade.php
+++ www/resources/views/Cardshop.blade.php
@@ -2,6 +2,74 @@
 
 @section('content')
 
-<h1>hyihyyshkihyiak</h1>
+<!-- cart-main-area start -->
+<div class="cart-main-area ptb--120 bg__white">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <form action="#">
+                        <div class="table-content table-responsive">
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th class="product-name">Product</th>
+                                        <th class="product-price">Price</th>
+                                        <th class="product-quantity">Quantity</th>
+                                        <th class="product-subtotal">Total</th>
+                                        <th class="product-remove">Remove</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td class="product-name"><a href="#">Vestibulum suscipit</a></td>
+                                        <td class="product-price"><span class="amount">£165.00</span></td>
+                                        <td class="product-quantity"><input type="number" value="1" /></td>
+                                        <td class="product-subtotal">£165.00</td>
+                                        <td class="product-remove"><a href="#">X</a></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="product-name"><a href="#">Vestibulum dictum magna</a></td>
+                                        <td class="product-price"><span class="amount">£50.00</span></td>
+                                        <td class="product-quantity"><input type="number" value="1" /></td>
+                                        <td class="product-subtotal">£50.00</td>
+                                        <td class="product-remove"><a href="#">X</a></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-8 col-sm-7 col-xs-12">
+                                <div class="buttons-cart">
+                                    <input type="submit" value="Update Cart" />
+                                    <a href="#">Continue Shopping</a>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-sm-5 col-xs-12">
+                                <div class="cart_totals">
+                                    <h2>Cart Totals</h2>
+                                    <table>
+                                        <tbody>
+                                            <tr class="order-total">
+                                                <th></th>
+                                                <td>
+                                                    <strong><span class="amount">£215.00</span></strong>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    <div class="wc-proceed-to-checkout">
+                                        <a href="checkout.html">Checkout</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- cart-main-area end -->
+
+    </div>
 
 @endsection
