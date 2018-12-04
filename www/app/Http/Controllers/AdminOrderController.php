93906ea6be4afe7b13602e3572143a6bc01e41b0
Eugene Kozlov <cyberkekc@gmail.com>
2018-12-05 01:45:00 +0300

    without composer

--- www/app/Http/Controllers/AdminOrderController.php
+++ www/app/Http/Controllers/AdminOrderController.php
@@ -19,28 +19,12 @@ class AdminOrderController extends Controller
 
     public function show($id)
     {
-//        if (isset($_POST['status-order-edit'])){
-//            OrderStatus::setStatus($id, $_POST['status-order-edit']);
-//        }
 
         $order = Order::getById($id);
-
-        if(empty($order)) {
-
-            dd('OOOOOPs');
-        }
-
         $allProds = Order::getProds($id);
         $ordHistory = Order::getOrdHistory($id);
-        dd($order);
-        return view(
-            'admin_order_show',
-            compact(
-                'order',
-                'allProds',
-                'ordHistory'
-            )
-        );
+        
+        return view('admin_order_show', compact('order','allProds', 'ordHistory'));
 
     }
 }
