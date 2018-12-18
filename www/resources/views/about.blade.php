2184fabe13701fdf36cc04684cc526459c7b7ad5
vkoyda <gbeleptica@gmail.com>
2018-12-18 13:10:14 +0300

    add_succes, about, cardshoop some front; fix remove bug

--- www/resources/views/about.blade.php
+++ www/resources/views/about.blade.php
@@ -3,17 +3,35 @@
 @section('content')
 
 <div class="container">
-    <h1 style="text-align: center">
-    Hello! 
-    <br><br><br>
-    </h1>
-<h2 style="text-align: justify">We are pleased to introduce our company to you!
-<br><br>
-Bublik company has been selling computers and components for them for more than 15 years.
-<br><br>
-Any of our employees will be happy to help you solve any problem related to the purchase of a computer and accessories.
-</h2>
-    
+    <div class="row">   
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <h1 style="text-align: center">
+                    Hello! 
+                <br><br><br>
+                </h1>
+                <div class="img">
+                    <img src="images/About_2.jpg" class="img-fluid" alt="Responsive image">
+                    <br><br><br>
+                </div>
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <h2 style="text-align: center">We are pleased to introduce our company to you!
+                <br><br>
+                Bublik company has been selling computers and components for them for more than 15 years.
+                <br><br>
+                Any of our employees will be happy to help you solve any problem related to the purchase of a computer and accessories.
+                </h2>
+                <br><br><br><br>
+                <h1 style="text-align: center">Thank you for choosing us!!!</h1>
+                <br><br>
+                <div class="img">
+                    <img src="images/About_1.jpg" class="img-fluid" alt="Responsive image">
+                    <br><br><br>
+                </div>
+            </div>  
+            </div>
+        </div>
+    </div>
 </div>
 
 @endsection
